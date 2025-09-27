036b2bb09d4f99038d6632104d5f621067fa3081
lijnati <lijnati3@gmail.com>
2025-09-27 22:04:18 +0300

    deploying: to ganache net

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,7 +16,12 @@ module.exports = {
       chainId: 1337
     },
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://127.0.0.1:7545"
+    },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: ["0xea60ca493ea10dec36a6f1ad72e175099812d0838ee16b8300572b0e6ec05059"]
     }
+    
   }
 };
