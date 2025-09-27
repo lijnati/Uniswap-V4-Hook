aa82e02ce04c6988140bb85424eb850bf5fe99fb
lijnati <lijnati3@gmail.com>
2025-09-27 22:15:50 +0300

    add:contract address

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const address = "DEPLOYED_CONTRACT_ADDRESS";
+  const address = "0x6d4889E0fA82d74E2DfcF47c86559EB12b85dbB7";
   const FeeCollectorHook = await ethers.getContractFactory("FeeCollectorHook");
   const contract = await FeeCollectorHook.attach(address);
 
