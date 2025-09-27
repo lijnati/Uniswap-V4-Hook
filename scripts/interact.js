a5e8282ab8078ebdbb44549ab2cbf7a42a27dbe8
lijnati <lijnati3@gmail.com>
2025-09-27 22:14:07 +0300

    fix:interact

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const address = "0xFbBa9C9956F14cc81C7B790106073f699E726102";
+  const address = "DEPLOYED_CONTRACT_ADDRESS";
   const FeeCollectorHook = await ethers.getContractFactory("FeeCollectorHook");
   const contract = await FeeCollectorHook.attach(address);
 
